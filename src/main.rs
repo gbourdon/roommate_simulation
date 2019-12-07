e26e7cd0fa3f9f182b6b4b6636021a7c6421220a
gbourdon <34389449+gbourdon@users.noreply.github.com>
2019-12-07 14:36:43 -0700

    Added basic simulation of students changing rooms

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
 use rand::prelude::*;
+
 fn shifting(switch_prob: f64) -> bool {
     let mut rng = rand::thread_rng();
     let switch: f64 = rng.gen();
     let switching: bool  =  switch <= switch_prob;
     switching
 }
+
 fn main() {
-    const SIMS_TO_RUN: u64 = 10000;
-    //const CONCURRENT_THREADS: u64 = 6;
-    //const NUMBER_OF_STUDENTS: u64 = 250;
-    const SWITCH_CHANCE: f64 = 0.25;
-    //let mut rng = thread_rng();
-    let mut num_stay = 0;
-    let mut num_shift = 0;
-    for _ in 0..SIMS_TO_RUN {
-        let switching: bool  =  shifting(SWITCH_CHANCE);
-        if switching {
-            num_shift += 1;
-        } else {
-            num_stay += 1;
+    const SIMS_TO_RUN: u64 = 100; // How many simulations do you want to run?
+    //const CONCURRENT_THREADS: u64 = 6; // How many do you want to run at once?
+    const NUMBER_OF_STUDENTS: u64 = 1000; // How many students (that matter) are "participating"?
+    const SWITCH_CHANCE: f64 = 0.25; // What is the chance that a student will change their roommate?
+
+    for sim_num in 0..SIMS_TO_RUN { // For each simulation
+        let mut num_stay = 0; // How many students are staying with their roommate?
+        let mut num_shift = 0; // And how many are changing roomates?
+        for _ in 0..NUMBER_OF_STUDENTS { // Finds if a student is keeping their roomate or changeing it
+            let switching: bool  =  shifting(SWITCH_CHANCE);
+            if switching {
+                num_shift += 1;
+            } else {
+                num_stay += 1;
+            }
         }
+        println!("SIM: {}: Staying: {}, Switching: {}", sim_num, num_stay, num_shift); // And gives the results
     }
-    println!("Staying: {}, Switching: {}", num_stay, num_shift);
 }
